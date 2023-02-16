7ceb8f6410312b20ef295c0607890d119cdcfd53
yauheni-parakhnevich <yauheni-parakhnevich@users.noreply.github.com>
2023-02-16 08:57:00 +0100

    Link to the product added

--- index.js
+++ index.js
@@ -15,16 +15,18 @@ exports.execute = (event, context) => {
         axios.get(httpCall).then(res => {
             const $ = cheerio.load(res.data);
         
-            var productName = $('article>a').attr('aria-label')
-            var imageUrl = `https://${site}.ch` + $('article>div>div:nth-child(1) img').attr('src')
-            var category = $('article>div>div:nth-child(2) a').text()
-            var priceNow = $('article>div>div:nth-child(2)>div:nth-child(2)>span:nth-child(1)').text()
-            var priceWas = $('article>div>div:nth-child(2)>div:nth-child(2)>span:nth-child(2)').text()
+            var productName = $('article>a').attr('aria-label');
+            var imageUrl = `https://${site}.ch` + $('article>div>div:nth-child(1) img').attr('src');
+            var category = $('article>div>div:nth-child(2) a').text();
+            var priceNow = $('article>div>div:nth-child(2)>div:nth-child(2)>span:nth-child(1)').text();
+            var priceWas = $('article>div>div:nth-child(2)>div:nth-child(2)>span:nth-child(2)').text();
+            var link = `https://${site}.ch` + $('article>a').attr('href');
             
             bodyMail = `<h1>${site}</h1>`
                 + `<p><strong>Category:</strong> ${category}</p>`
                 + `<p><strong>Product:</strong> ${productName}</p>`
                 + `<p><strong>Price:</strong> ${priceNow} was ${priceWas}</p>`
+                + `<p><a href='${link}'>Link</a></p>`
                 + `<p><img src="${imageUrl}" alt="${productName}"></p>`
             
             const mailjetRequest = mailjet.post("send", { 'version': 'v3.1' })
